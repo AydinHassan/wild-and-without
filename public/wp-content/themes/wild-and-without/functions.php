7883afa6c3c8415280b8337cd9abb26cdc5cd30b
Aydin Hassan <aydin@hotmail.co.uk>
2017-08-25 22:01:18 +0200

    Add adsense

--- public/wp-content/themes/wild-and-without/functions.php
+++ public/wp-content/themes/wild-and-without/functions.php
@@ -12,3 +12,15 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+add_action('wp_head', function () {
+    ?>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-9221923268071583",
+            enable_page_level_ads: true
+        });
+    </script>
+    <?php
+});
