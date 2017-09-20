f1bf0e5f0a4d5fdd9cf6dc95f22d7a224f24cb3e
Aydin Hassan <aydin@hotmail.co.uk>
2017-09-20 14:50:48 +0200

    Child theme

--- public/wp-content/themes/wild-and-without/functions.php
+++ public/wp-content/themes/wild-and-without/functions.php
@@ -1,17 +1,8 @@
 <?php
-function my_theme_enqueue_styles() {
 
-    $parent_style = 'twentyseventeen-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-
-    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-    wp_enqueue_style(
-        'wild-and-without-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        [$parent_style],
-        wp_get_theme()->get('Version')
-    );
-}
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('voyager-parent-style', get_template_directory_uri() . '/style.css');
+});
 
 add_action('wp_head', function () {
     ?>
