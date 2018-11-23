ae70cb2e07ead5157436e535c0e6ebd83dfac736
Aydin Hassan <aydin@hotmail.co.uk>
2018-11-23 18:26:06 +0300

    Try fix deprecations

--- public/wp-content/themes/voyager/framework/widgets/widget-social.php
+++ public/wp-content/themes/voyager/framework/widgets/widget-social.php
@@ -105,6 +105,7 @@ class cstheme_widget_sociallinks extends WP_Widget {
         }
     }
 }
-
-add_action('widgets_init', create_function('', 'return register_widget("cstheme_widget_sociallinks");'));
+add_action('widgets_init', function () {
+    return register_widget("cstheme_widget_sociallinks");
+});
 ?>
