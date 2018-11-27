5a31750f2ffc070eee5385d990a326f85684cc9e
Aydin Hassan <aydin@hotmail.co.uk>
2018-11-28 00:37:14 +0300

    Fix html

--- public/wp-content/themes/wild-and-without/single.php
+++ public/wp-content/themes/wild-and-without/single.php
@@ -25,71 +25,71 @@ $featured_image_url = wp_get_attachment_url(get_post_thumbnail_id());
 
         <div id="blog-single-wrap" class="<?php echo 'format-' . $pf . ' featured_img_' . $single_post_featured_img ?> clearfix">
 
-        <div class="single_post_header">
-            <div class="featured_img_bg" style="background-image:url(<?php echo $featured_image_url; ?>);"></div>
-            <div class="single_post_meta_category"><?php the_category(', '); ?></div>
-            <h2 class="single-post-title"><?php the_title(); ?></h2>
-            <div class="single_post_header_bottom">
-                <div class="single_post_meta">
-                    <span class="post-meta-date"><?php the_time('M j, Y') ?></span>
-                    <span class="post_meta_views"><i class="fa fa-eye"></i> <span><?php echo (get_post_meta(get_the_ID(), "post_views", true) > 0 ? get_post_meta(get_the_ID(), "post_views", true) : "0"); ?></span></span>
-                    <span class="post-meta-likes"><?php echo cstheme_likes(); ?></span>
-                    <span class="post-meta-comments"><i class="fa fa-comments"></i><?php echo get_comments_number(get_the_ID()); ?></span>
+            <div class="single_post_header">
+                <div class="featured_img_bg" style="background-image:url(<?php echo $featured_image_url; ?>);"></div>
+                <div class="single_post_meta_category"><?php the_category(', '); ?></div>
+                <h2 class="single-post-title"><?php the_title(); ?></h2>
+                <div class="single_post_header_bottom">
+                    <div class="single_post_meta">
+                        <span class="post-meta-date"><?php the_time('M j, Y') ?></span>
+                        <span class="post_meta_views"><i class="fa fa-eye"></i> <span><?php echo (get_post_meta(get_the_ID(), "post_views", true) > 0 ? get_post_meta(get_the_ID(), "post_views", true) : "0"); ?></span></span>
+                        <span class="post-meta-likes"><?php echo cstheme_likes(); ?></span>
+                        <span class="post-meta-comments"><i class="fa fa-comments"></i><?php echo get_comments_number(get_the_ID()); ?></span>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-offset-2 col-md-8">
-                <div class="single-post-content clearfix">
+            <div class="row">
+                <div class="col-md-offset-2 col-md-8">
+                    <div class="single-post-content clearfix">
 
-                    <?php
-                    the_content(esc_html__('Read more!', 'voyager'));
-                    wp_link_pages(array('before' => '<div class="page-link">' . esc_html__('Pages', 'voyager') . ': ', 'after' => '</div>'));
-                    ?>
-
-                </div>
-
-                <div class="posts_nav_link"><?php posts_nav_link(); ?></div>
-
-                <div class="single_sharebox_wrap clearfix">
-                    <div class="single_post_meta_tags pull-left">
-
-                        <?php if( has_tag() ) {
-                            the_tags('','', '');
-                        } ?>
-
-                    </div>
-
-                    <div class="pull-right">
-
-                        <?php if(cstheme_option('single_post_sharebox') != 0) { get_template_part( 'templates/blog/sharebox' ); } ?>
-
-                    </div>
-                </div>
-
-                <?php if(cstheme_option('single_post_authorinfo') != 0) { get_template_part( 'templates/blog/authorinfo' ); } ?>
-
-                <?php if(cstheme_option('single_post_navigation') != 0) { ?>
-                    <div class="single_post_nav clearfix">
                         <?php
-                        $prev_post = get_adjacent_post(false, '', true);
-                        $next_post = get_adjacent_post(false, '', false);
-
-                        if($prev_post){
-                            $post_url = get_permalink($prev_post->ID);
-                            echo '<div class="pull-left"><a href="' . esc_url( $post_url ) . '" title="' . $prev_post->post_title . '"><p class="heading_font"><i class="fa fa-chevron-left"></i>' . esc_html__('Previous','voyager') . '</p><b>' . $prev_post->post_title . '</b></a></div>';
-                        }
-
-                        if($next_post) {
-                            $post_url = get_permalink($next_post->ID);
-                            echo '<div class="pull-right text-right"><a href="' . esc_url( $post_url ) . '" title="' . $next_post->post_title . '"><p class="heading_font">' . esc_html__('Next','voyager') . '<i class="fa fa-chevron-right"></i></p><b>' . $next_post->post_title . '</b></a></div>';
-                        }
+                        the_content(esc_html__('Read more!', 'voyager'));
+                        wp_link_pages(array('before' => '<div class="page-link">' . esc_html__('Pages', 'voyager') . ': ', 'after' => '</div>'));
                         ?>
+
                     </div>
-                <?php } ?>
+
+                    <div class="posts_nav_link"><?php posts_nav_link(); ?></div>
+
+                    <div class="single_sharebox_wrap clearfix">
+                        <div class="single_post_meta_tags pull-left">
+
+                            <?php if( has_tag() ) {
+                                the_tags('','', '');
+                            } ?>
+
+                        </div>
+
+                        <div class="pull-right">
+
+                            <?php if(cstheme_option('single_post_sharebox') != 0) { get_template_part( 'templates/blog/sharebox' ); } ?>
+
+                        </div>
+                    </div>
+
+                    <?php if(cstheme_option('single_post_authorinfo') != 0) { get_template_part( 'templates/blog/authorinfo' ); } ?>
+
+                    <?php if(cstheme_option('single_post_navigation') != 0) { ?>
+                        <div class="single_post_nav clearfix">
+                            <?php
+                            $prev_post = get_adjacent_post(false, '', true);
+                            $next_post = get_adjacent_post(false, '', false);
+
+                            if($prev_post){
+                                $post_url = get_permalink($prev_post->ID);
+                                echo '<div class="pull-left"><a href="' . esc_url( $post_url ) . '" title="' . $prev_post->post_title . '"><p class="heading_font"><i class="fa fa-chevron-left"></i>' . esc_html__('Previous','voyager') . '</p><b>' . $prev_post->post_title . '</b></a></div>';
+                            }
+
+                            if($next_post) {
+                                $post_url = get_permalink($next_post->ID);
+                                echo '<div class="pull-right text-right"><a href="' . esc_url( $post_url ) . '" title="' . $next_post->post_title . '"><p class="heading_font">' . esc_html__('Next','voyager') . '<i class="fa fa-chevron-right"></i></p><b>' . $next_post->post_title . '</b></a></div>';
+                            }
+                            ?>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
-        </div>
 
             <?php get_template_part('templates/blog/related-posts') ?>
 
