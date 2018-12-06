f349c7820fd095bfdc99da9453aeb0421792e272
Aydin Hassan <aydin@hotmail.co.uk>
2018-12-06 15:09:09 +0400

    Initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    entry: "./public/wp-content/themes/wild-and-without/index.js",
+    output: {
+        path: path.resolve('./public/wp-content/themes/wild-and-without', 'dist'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|gif|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.scss|.css$/,
+                use:  [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        // new OptimizeCssAssetsPlugin({
+        //     assetNameRegExp: /\.optimize\.css$/g,
+        //     cssProcessor: require('cssnano'),
+        //     cssProcessorPluginOptions: {
+        //         preset: ['default', { discardComments: { removeAll: true } }],
+        //     },
+        //     canPrint: true
+        // })
+    ]
+}
