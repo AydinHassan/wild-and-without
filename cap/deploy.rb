b6175ede59fd40dc6ae09d90833fddf3239cf039
Aydin Hassan <aydin@hotmail.co.uk>
2018-12-06 14:53:30 +0400

    Share cache files

--- cap/deploy.rb
+++ cap/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, '/var/www/wildandwithout.com'
 set :pty, true
 set :log_level, :debug
 set :keep_releases, 3
-set :linked_files, %w{.env robots.txt}
+set :linked_files, %w{.env robots.txt public/wp-content/db.php public/wp-content/object-cache.php public/wp-content/advanced-cache.php}
 set :linked_dirs, %w(public/wp-content/uploads public/wp-content/w3tc-config public/wp-content/cache backup)
 
 namespace :deploy do
