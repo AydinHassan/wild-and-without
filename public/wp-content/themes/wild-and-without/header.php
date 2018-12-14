99314689adfe7ac9933b21dac2486fa5f3c586fd
Aydin Hassan <aydin@hotmail.co.uk>
2018-12-14 16:11:10 +0400

    Add booking.com meta tag

--- public/wp-content/themes/wild-and-without/header.php
+++ public/wp-content/themes/wild-and-without/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+    <meta name="B-verify" content="a19d7f8703389d3156762141f6f25cfa5fd9906d" />
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
