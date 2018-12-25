e33c9eb2696c230e035cca70d9bc705ff3138b5d
Aydin Hassan <aydin@hotmail.co.uk>
2018-12-25 22:01:55 +0400

    Remove googleads

--- public/wp-content/themes/wild-and-without/header.php
+++ public/wp-content/themes/wild-and-without/header.php
@@ -10,13 +10,6 @@
         var cstheme_ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
     </script>
 	<?php wp_head(); ?>
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-2116111520209612",
-            enable_page_level_ads: true
-        });
-    </script>
 </head>
 <body <?php body_class(); ?>>
 	
