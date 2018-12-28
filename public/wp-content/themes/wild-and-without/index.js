6f11bba7a5206005fa673a31b146447d9104e52d
Aydin Hassan <aydin@hotmail.co.uk>
2018-12-28 18:43:33 +0400

    Extract content form css

--- public/wp-content/themes/wild-and-without/index.js
+++ public/wp-content/themes/wild-and-without/index.js
@@ -2,10 +2,8 @@ import './css/bootstrap.css';
 import '../voyager/style.css';
 import '../voyager/css/theme-style.css';
 import '../voyager/css/responsive.css';
-import '../../plugins/contact-form-7/includes/css/styles.css'
 import './css/main.scss';
 import '../../../wp/wp-includes/css/dist/block-library/style.css';
 
-//import '../../plugins/contact-form-7/includes/js/scripts.js';
 import './js/wildandwithout.js';
 
