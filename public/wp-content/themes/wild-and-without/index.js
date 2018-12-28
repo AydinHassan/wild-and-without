fcd0c0ba2ef4903fff7557811b66796a81162c19
Aydin Hassan <aydin@hotmail.co.uk>
2018-12-28 15:59:38 +0400

    Extract only used CSS from swiper

--- public/wp-content/themes/wild-and-without/index.js
+++ public/wp-content/themes/wild-and-without/index.js
@@ -3,7 +3,6 @@ import '../voyager/style.css';
 import '../voyager/css/theme-style.css';
 import '../voyager/css/responsive.css';
 import '../../plugins/contact-form-7/includes/css/styles.css'
-import 'swiper/dist/css/swiper.css';
 import './css/main.scss';
 import '../../../wp/wp-includes/css/dist/block-library/style.css';
 
