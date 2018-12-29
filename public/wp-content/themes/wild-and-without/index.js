5b8048c5c27ab796816e3ace7bebf99cd4be564e
Aydin Hassan <aydin@hotmail.co.uk>
2018-12-29 19:50:37 +0400

    Refactor all CSS

--- public/wp-content/themes/wild-and-without/index.js
+++ public/wp-content/themes/wild-and-without/index.js
@@ -1,7 +1,4 @@
 import './css/bootstrap.css';
-import '../voyager/style.css';
-import '../voyager/css/theme-style.css';
-import '../voyager/css/responsive.css';
 import './css/main.scss';
 import '../../../wp/wp-includes/css/dist/block-library/style.css';
 
