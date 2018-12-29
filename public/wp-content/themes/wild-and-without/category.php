16273ef3ba61397df15a8f900ead1e92829858da
Aydin Hassan <aydin@hotmail.co.uk>
2018-12-29 23:20:37 +0400

    Category featured image

--- public/wp-content/themes/wild-and-without/category.php
+++ public/wp-content/themes/wild-and-without/category.php
@@ -6,12 +6,21 @@
 get_header();
 
 ?>
-
+<?php $category = get_queried_object(); ?>
 		
 <div id="blog_list" class="container mt0 no_sidebar blog_list_style_grid-bg columns3 clearfix">
-    <div class="page_title text-center">
-        <h1><?php printf( esc_html__( '%s', 'voyager' ), single_cat_title( '', false ) ); ?></h1>
-    </div>
+
+    <?php if ($category->description) : ?>
+        <div class="category-feature">
+            <img class="featured-image" src="<?= z_taxonomy_image_url($category->Cat_ID) ?>"/>
+            <div class="category-info">
+                <div class="page_title text-center">
+                    <h1><?php printf( esc_html__( '%s', 'voyager' ), single_cat_title( '', false ) ); ?></h1>
+                </div>
+                <p class="description text-justify"><?= $category->description; ?></p>
+            </div>
+        </div>
+    <?php endif ?>
     <div class="row">
         <?php while (have_posts()) : ?>
             <?php the_post(); ?>
