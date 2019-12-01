a455180c3ae082c74c53d246bff54d11d61ba368
Aydin Hassan <aydin@hotmail.co.uk>
2019-12-01 20:19:48 +0100

    Clean header

--- public/wp-content/themes/wild-and-without/header.php
+++ public/wp-content/themes/wild-and-without/header.php
@@ -13,26 +13,8 @@
 </head>
 <body <?php body_class(); ?>>
 	
-	<?php if( cstheme_option( 'fixed_sidebar_enable') ) { ?>
-		<div class="fixed-sidebar">
-			<a class="sidebar_btn_close" href="javascript:void(0);"></a>
-			<div class="scroll-wrap"><?php get_sidebar('fixed'); ?></div>
-		</div>
-	<?php } ?>
-	
-	<?php if( cstheme_option( 'subscribe_popup_enable') ) {
-		get_template_part( 'templates/subscribe-popup' );
-	} ?>
-	
-	<?php if ( cstheme_woo_enabled() && cstheme_option( 'mini_cart_enabled') ) {
-		echo cstheme_get_cart_icon();
-		get_template_part( 'templates/mini-cart' );
-	} ?>
-	
+	<?php get_template_part('templates/subscribe-popup') ?>
 	<div id="page-wrap">
-		
-		<?php get_template_part( 'templates/header/header_layout' ); ?>
-		
-		<?php get_template_part( 'templates/header/header-mobile' ); ?>
-		
+		<?php get_template_part('templates/header/header_layout') ?>
+		<?php get_template_part('templates/header/header-mobile') ?>
 		<div id="page-content">
