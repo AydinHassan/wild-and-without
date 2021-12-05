4c79d3d3c84deca18feee9651e0a5caf00a40869
Aydin Hassan <aydin@hotmail.co.uk>
2021-12-05 18:38:07 +0000

    Improve header when scrolling + fix search results

--- public/wp-content/themes/wild-and-without/templates/header/header_layout.php
+++ public/wp-content/themes/wild-and-without/templates/header/header_layout.php
@@ -5,7 +5,7 @@
                     <div class="col-md-3 logo_wrap_col">
                         <?php cstheme_logo(); ?>
                     </div>
-                    <div class="col-md-7 text-center">
+                    <div class="col-md-7 text-center header_container">
                         <div class="menu-primary-menu-container-wrap heading_font clearfix">
                             <a class="mobile_menu_btn" href="javascript:void(0)"><?php echo esc_html__( 'Menu', 'voyager') ?></a>
                             <?php wp_nav_menu(['menu_class' => 'nav-menu', 'theme_location' => 'primary', 'depth' => 3, 'container' => false, 'fallback_cb' => 'cstheme_Walker_Nav_Menu_Edit_Custom::fallback', 'walker' => new cstheme_MegaMenu_Walker]); ?>
@@ -22,6 +22,11 @@
                         <?php } ?>
                         <div class="header_search pull-right">
                             <?php get_search_form(true) ?>
+                        </div>
+                    </div>
+                    <div class="col-md-offset-1 col-md-2">
+                        <div class="social_links_wrap social_links_wrap_fixed">
+                            <?= cstheme_social_links(); ?>
                         </div>
                     </div>
                 </div>
