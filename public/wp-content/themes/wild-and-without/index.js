49ec3aa44c732ceffc00b0cc8413231059a43ebf
Aydin Hassan <aydin@hotmail.co.uk>
2022-05-09 10:59:37 +0200

    Try fix wide/full image galleries

--- public/wp-content/themes/wild-and-without/index.js
+++ public/wp-content/themes/wild-and-without/index.js
@@ -1,7 +1,6 @@
 import './css/bootstrap.css';
 import './css/main.scss';
 import '../../../wp/wp-includes/css/dist/block-library/style.css';
-import '../../../wp-content/plugins/instagram-feed/css/sbi-styles.min.css';
 
 import './js/wildandwithout.js';
 
