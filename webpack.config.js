9faf7845e0e7021eb346ca55cef0dbbc9af8cc03
Aydin Hassan <aydin@hotmail.co.uk>
2022-11-18 16:26:32 +0100

    Upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -47,13 +47,10 @@ module.exports = {
             },
             {
                 test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts/'
-                    }
-                }]
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                },
             }
         ]
     },
