95f15702196a8ebcc90f1f52c7dc2a6b1bc71618
Aydin Hassan <aydin@hotmail.co.uk>
2022-11-24 12:34:32 +0100

    Remove align support

--- public/wp-content/plugins/hike-stats/build/index.asset.php
+++ public/wp-content/plugins/hike-stats/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '349edc042dd9337870d1');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '00c84c83d2de354a19f0');
