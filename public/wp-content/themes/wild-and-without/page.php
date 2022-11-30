7fdb3b31bf8ebe0a1052d1d902e1b485097b64ec
Aydin Hassan <aydin@hotmail.co.uk>
2022-11-30 11:01:41 +0100

    Remove sidebar support

--- public/wp-content/themes/wild-and-without/page.php
+++ public/wp-content/themes/wild-and-without/page.php
@@ -1,12 +1,11 @@
 <?php get_header() ?>
-<?php $sidebar = get_metabox('sidebar_layout') === 'no-sidebar' ? null : get_metabox('sidebar_layout') ?>
     <div id="default_page">
         <?php if (!empty($featuredImageUrl = wp_get_attachment_url(get_post_thumbnail_id()))) : ?>
             <div class="page_featured_image" style="background-image:url(' <?= $featuredImageUrl ?>');"></div>
         <?php endif ?>
         <div class="container">
             <div class="row">
-                <div class="<?= $sidebar ? ($sidebar === 'left-sidebar' ? 'col-md-9 pull-right' : 'col-md-9 pull-left') : 'col-md-12' ?>">
+                <div class="col-md-12">
                     <div class="page_title text-center">
                         <h1><?php the_title(); ?></h1>
                     </div>
@@ -25,11 +24,6 @@
                         <?php endif; ?>
                     </div>
                 </div>
-                <?php if ($sidebar) : ?>
-                    <div class="col-md-3 pull-<?= $sidebar === 'left-sidebar' ? 'left' : 'right' ?>">
-                        <?php get_sidebar() ?>
-                    </div>
-                <?php endif ?>
             </div>
         </div>
     </div>
